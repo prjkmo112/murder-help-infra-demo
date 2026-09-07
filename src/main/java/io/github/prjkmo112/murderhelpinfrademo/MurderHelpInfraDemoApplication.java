b5b17fc72c0d5f3cdf787b08fc33e26138e656e5
momo <ptjkjm1@gmail.com>
2026-09-07 16:43:00 +0900

    Initial commit

---
+++ src/main/java/io/github/prjkmo112/murderhelpinfrademo/MurderHelpInfraDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.prjkmo112.murderhelpinfrademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurderHelpInfraDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MurderHelpInfraDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/prjkmo112/murderhelpinfrademo/MurderHelpInfraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.prjkmo112.murderhelpinfrademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurderHelpInfraDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
